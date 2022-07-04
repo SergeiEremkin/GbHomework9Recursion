4b8a4e20dd7f9fbcbf9c752fb7120effaf66af15
SergeiEremkin <ffr@list.ru>
2022-07-04 23:42:35 +0300

    Добавил задания по домашке 9

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,1 +1,12 @@
-﻿
+﻿//Задача 66
+int SumElements(int M, int N)
+{
+    if (M<N)
+    {
+         M += SumElements(M+1, N);
+    }
+    return M;
+}
+System.Console.WriteLine(SumElements(1,10));
+System.Console.WriteLine(SumElements(1,2));
+System.Console.WriteLine(SumElements(5,7));
--- Task2/Program.cs
+++ Task2/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿static int AckermannFunc(int m, int n)
+{
+    if (m == 0)
+    {
+        return n + 1;
+    }
+    else if ((m > 0) && (n == 0))
+    {
+        return AckermannFunc(m - 1, 1);
+    }
+    else if ((m > 0) && (n > 0))
+    {
+        return AckermannFunc(m - 1, AckermannFunc(m, n - 1));
+    }
+    else
+        return n + 1;
+}
+Console.WriteLine(AckermannFunc(3,2));
+Console.WriteLine(AckermannFunc(2,3));
+Console.WriteLine(AckermannFunc(0,3));
+Console.WriteLine(AckermannFunc(4,0));
